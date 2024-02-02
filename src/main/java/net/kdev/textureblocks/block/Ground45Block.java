9fde85595e4b49edafbca556c9b62c81824113c9
Kisonix <124847984+Kisonix-Dev@users.noreply.github.com>
2024-02-02 19:26:42 +0300

    Update
    - Added 30 blocks with texture of Granite.
    - Localization of blocks has been carried out: Granite into Russian.
    - Added 46 blocks with texture of Ground.
    - Localization of blocks has been carried out: Ground into Russian.
    - Added 10 blocks with texture of Dirt.
    - Localization of blocks has been carried out: Dirt into Russian.
    - The name was changed from ground 23 to ground 3.

---
+++ src/main/java/net/kdev/textureblocks/block/Ground45Block.java
@@ -0,0 +1,71 @@
+
+package net.kdev.textureblocks.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.kdev.textureblocks.itemgroup.TextureBlocksItemGroup;
+import net.kdev.textureblocks.TextureBlocksModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@TextureBlocksModElements.ModElement.Tag
+public class Ground45Block extends TextureBlocksModElements.ModElement {
+	@ObjectHolder("texture_blocks:ground_45")
+	public static final Block block = null;
+
+	public Ground45Block(TextureBlocksModElements instance) {
+		super(instance, 321);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(TextureBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(1.5f, 6f).setLightLevel(s -> 0)
+					.harvestLevel(1).harvestTool(ToolType.SHOVEL).setRequiresTool().notSolid().setOpaque((bs, br, bp) -> false));
+			setRegistryName("ground_45");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
